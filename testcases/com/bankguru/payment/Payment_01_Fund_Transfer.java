c5245b893701424a268477451d84712430235091
le anh tuan <leanhtuan2425@gmail.com>
2021-03-04 01:31:13 +0700

    Init new automation test framwork

---
+++ testcases/com/bankguru/payment/Payment_01_Fund_Transfer.java
@@ -0,0 +1,40 @@
+package com.bankguru.payment;
+
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.AfterClass;
+
+public class Payment_01_Fund_Transfer {
+	@Test
+	public void New_Customer_01_Validate_Name() {
+
+	}
+
+	@Test
+	public void New_Customer_02_Validate_Phone() {
+
+	}
+
+	@Test
+	public void New_Customer_03_Validate_City() {
+
+	}
+
+	@Test
+	public void New_Customer_04_Validate_State() {
+		
+	}
+	@Test
+	public void New_Customer_05_Validate_Address() {
+
+	}
+
+	@BeforeClass
+	public void beforeClass() {
+	}
+
+	@AfterClass
+	public void afterClass() {
+	}
+
+}
